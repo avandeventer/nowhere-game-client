463de3c4740d1a9c500a1af1b913eaddaa753a8b
avandeventer <vandeventerandy@gmail.com>
2024-12-01 19:42:05 -0500

    Changing adventure display

--- src/assets/location.ts
+++ src/assets/location.ts
@@ -1,9 +1,15 @@
+import { Option } from './option';
+
 export class Location {
     locationId: number;
     locationName: String;
+    options: Option[];
+    label: String;
 
     constructor () {
         this.locationId = 0;
         this.locationName = "";
+        this.options = []
+        this.label = "";
     }
 }
