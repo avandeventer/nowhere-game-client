9ca8d73cd59f639b6f48de7cd1e556680db2424d
null <null>
2025-07-30 23:32:46 -0400

    Simplified player turn logic by putting turn changes on the player client and the backend

--- src/adventure/adventure.component.ts
+++ src/adventure/adventure.component.ts
@@ -1,10 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, ElementRef, EventEmitter, Input, OnInit, Output, SimpleChanges, ViewChild } from "@angular/core";
+import { Component, ElementRef, Input, OnInit, Output, SimpleChanges, ViewChild } from "@angular/core";
 import { ActivePlayerSession } from "src/assets/active-player-session";
 import { GameState } from "src/assets/game-state";
 import { Player } from "src/assets/player";
 import { Location } from "src/assets/location";
-import { Story } from 'src/assets/story';
 import { environment } from 'src/environments/environments';
 import { HttpConstants } from 'src/assets/http-constants';
 import {MatCardModule} from '@angular/material/card';
@@ -25,8 +24,7 @@ export class AdventureComponent implements OnInit {
     locations: Location[] = [];
     players: Player[] = [];
     currentPlayerIndex: number = 0;
-    playerName: String = "";
-    playerTurnAuthorId: String = "";
+    currentTurnPlayer: Player | undefined = new Player();
     roundNumber: number = 0;
     settingNextPlayerTurn: boolean = false;
 
@@ -40,44 +38,29 @@ export class AdventureComponent implements OnInit {
     ngOnChanges(changes: SimpleChanges): void {
       if (changes['activePlayerSession']) {
         setTimeout(() => this.scrollToBottom(), 0);
-    
-        if (changes['activePlayerSession'].currentValue?.setNextPlayerTurn && !this.settingNextPlayerTurn) {
-          this.settingNextPlayerTurn = true;
-          this.setNextPlayerTurn();
+
+        if (
+            this.activePlayerSession.playerId !== null
+            && this.activePlayerSession.playerId !== ""
+            && this.activePlayerSession.playerId !== this.currentTurnPlayer?.authorId
+        ) {
+          this.setCurrentPlayer();
         }
       }
-    }    
+    }
 
     private scrollToBottom(): void {
       try {
-        this.scrollContainer.nativeElement.scrollTop = this.scrollContainer.nativeElement.scrollHeight;
+        if (this.scrollContainer !== undefined) {
+          this.scrollContainer.nativeElement.scrollTop = this.scrollContainer.nativeElement.scrollHeight;
+        }
       } catch (err) {
         console.error("Failed to scroll", err);
       }
     }
   
-    setNextPlayerTurn() {
-      console.log("Set next player's turn");
-      console.log("Player turn, round number", this.currentPlayerIndex, this.roundNumber);
-      if(this.currentPlayerIndex < this.players.length) {
-        this.playerTurnAuthorId = this.players[this.currentPlayerIndex].authorId;
-        this.playerName = this.players[this.currentPlayerIndex].userName;
-        this.currentPlayerIndex++;
-        console.log("Player turn index, round number, author id", 
-          this.currentPlayerIndex, 
-          this.roundNumber, 
-          this.playerTurnAuthorId);
-        this.updateActivePlayerSession(new Story(), "", [], this.playerTurnAuthorId, false, "", []);
-        this.settingNextPlayerTurn = false;
-      } else {
-        if(this.roundNumber <= 3) {
-          this.roundNumber++;
-          this.currentPlayerIndex = 0;
-          this.setNextPlayerTurn();
-        } else {
-          this.roundNumber = 0;
-        }
-      }
+    setCurrentPlayer() {
+      this.currentTurnPlayer = this.players.find(player => player.authorId === this.activePlayerSession.playerId);
     }
 
     getPlayers() {
@@ -93,12 +76,9 @@ export class AdventureComponent implements OnInit {
           next: (response) => {
             console.log('Players retrieved!', response);
             this.players = response;
-            if (this.activePlayerSession.playerId === "") {
-              this.setNextPlayerTurn();
-            } else {
-              this.setExistingPlayerTurn(this.activePlayerSession.playerId);
-            }
+            this.setCurrentPlayer();
             console.log('Players:', this.players);
+            console.log('Current Player:', this.currentTurnPlayer);
           },
           error: (error) => {
             console.error('Error getting players', error);
@@ -108,53 +88,18 @@ export class AdventureComponent implements OnInit {
 
   setExistingPlayerTurn(playerId: String) {
     this.currentPlayerIndex = this.players.findIndex((player) => player.authorId === playerId);
-    this.playerName = this.players[this.currentPlayerIndex].userName;
-  }
-      
-  private updateActivePlayerSession(
-    playerStory: Story,
-    selectedOptionId: String,
-    outcomeDisplay: String[],
-    playerStoryId: String,
-    setNextPlayerTurn: boolean,
-    selectedLocationOptionId: String,
-    locationOutcomeDisplay: String[]
-  ) {
-    console.log("Your player session", this.activePlayerSession);
-    const newActivePlayerSession: ActivePlayerSession = new ActivePlayerSession();
-    newActivePlayerSession.gameCode = this.gameCode;
-    newActivePlayerSession.playerId = playerStoryId;
-    newActivePlayerSession.story = playerStory;
-    newActivePlayerSession.outcomeDisplay = outcomeDisplay;
-    newActivePlayerSession.playerChoiceOptionId = selectedOptionId;
-    newActivePlayerSession.setNextPlayerTurn = setNextPlayerTurn;
-    newActivePlayerSession.selectedLocationOptionId = selectedLocationOptionId;
-    newActivePlayerSession.locationOutcomeDisplay = locationOutcomeDisplay;
-    console.log("New player session", newActivePlayerSession);
-
-    this.activePlayerSession.playerId = playerStoryId;
-
-    this.http
-      .put<ActivePlayerSession>(environment.nowhereBackendUrl + HttpConstants.ACTIVE_PLAYER_SESSION_PATH, newActivePlayerSession)
-      .subscribe({
-        next: (response) => {
-          console.log('Active player session updated!', response);
-        },
-        error: (error) => {
-          console.error('Error updating session', error);
-        },
-      });
+    this.currentTurnPlayer = this.players[this.currentPlayerIndex];
   }
 
-  playerTurnMessage(): String {
+  playerTurnMessage(): String {    
     switch(this.gameState) {
       case GameState.ROUND1:
       case GameState.ROUND2:
-        return `${this.playerName} is seeking adventure. Choose what you'll do from your phone.`;
+        return `${this.currentTurnPlayer?.userName} is seeking adventure. Choose what you'll do from your phone.`;
       case GameState.RITUAL:
-        return `${this.playerName} is making their final choice. Choose what you'll do from your phone.`;
+        return `${this.currentTurnPlayer?.userName} is making their final choice. Choose what you'll do from your phone.`;
       case GameState.ENDING:
-        return `${this.playerName}'s fate is sealed`
+        return `${this.currentTurnPlayer?.userName}'s fate is sealed`
       default:
         return "Look to your phone for answers!"
 
--- src/assets/http-constants.ts
+++ src/assets/http-constants.ts
@@ -5,6 +5,7 @@ export abstract class HttpConstants {
     static readonly GAME_SESSION_PATH = "/game";
     static readonly NEXT_GAME_SESSION_PATH = "/game/next";
     static readonly ACTIVE_PLAYER_SESSION_PATH = "/activePlayerSession";
+    static readonly ACTIVE_PLAYER_SESSION_NEXT_PATH = "/activePlayerSession/next";
     static readonly PLAYER_STORIES_PLAYED_PATH: string = "/adventure/played";
     static readonly PLAYER_AUTHORID_PATH: string = "/player/authorId";
     static readonly ACTIVE_GAME_STATE_SESSION_PATH: string = "/activeGameStateSession";
