4e1d4f348715c16d9814bc9b1b957aaf11689337
LAPTOP-NDGIGSQQ\vande <vandeventerandy@gmail.com>
2025-08-28 00:19:23 -0400

    Managing game state changes has been moved to the backend so removing it from the game client.

--- src/game-state-manager/game-state-manager.component.ts
+++ src/game-state-manager/game-state-manager.component.ts
@@ -1,15 +1,11 @@
 import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges } from '@angular/core';
 import { GameService } from '../services/game-session.service';
-import { HttpClient } from '@angular/common/http';
 import { GameState } from 'src/assets/game-state';
 import { Location } from 'src/assets/location';
 import { WritePromptComponent } from 'src/write-prompt/write-prompt.component';
 import { AdventureComponent } from 'src/adventure/adventure.component';
 import { ActivePlayerSession } from 'src/assets/active-player-session';
 import { ActiveGameStateSession } from 'src/assets/active-game-state-session';
-import { environment } from 'src/environments/environments';
-import { HttpConstants } from 'src/assets/http-constants';
-import { debounceTime, distinctUntilChanged } from 'rxjs';
 import { LocationComponent } from 'src/location/location.component';
 import { FinaleComponent } from 'src/finale/finale.component';
 import { GameSessionDisplay } from 'src/assets/game-session-display';
@@ -29,7 +25,6 @@ export class GameStateManagerComponent implements OnInit {
   activePlayerSession: ActivePlayerSession = new ActivePlayerSession();
   activeGameStateSession: ActiveGameStateSession = new ActiveGameStateSession();
   didWeSucceed: boolean = false;
-  isSettingNextGameState: boolean = false;
   gameSessionDisplay: GameSessionDisplay = new GameSessionDisplay();
   adventureMap: AdventureMap = new AdventureMap();
   currentLocation: Location = new Location();
@@ -41,18 +36,15 @@ export class GameStateManagerComponent implements OnInit {
     this.activePlayerSession = new ActivePlayerSession();
     this.activeGameStateSession = new ActiveGameStateSession();
     this.didWeSucceed = false;
-    this.isSettingNextGameState = false;  
   }
 
   constructor(
-    private gameService: GameService,
-    private http: HttpClient
+    private gameService: GameService
   ) {}
 
   ngOnInit() {
     this.populateGameSessionDisplay(this.gameCode);
     this.gameService.listenForGameStateChanges(this.gameCode)
-      .pipe(debounceTime(300), distinctUntilChanged((a, b) => a.activeGameStateSession === b.activeGameStateSession))
       .subscribe((newState) => {
       this.gameState = newState.gameState as unknown as GameState;
       this.activePlayerSession = newState.activePlayerSession as unknown as ActivePlayerSession;
@@ -62,9 +54,8 @@ export class GameStateManagerComponent implements OnInit {
       this.activeGameStateSession.isPlayerDone = new Map(Object.entries(rawIsPlayerDone));
       this.didWeSucceed = newState.didWeSucceed;
   
+      console.log('New game state received:', this.gameState);
       console.log('New adventureMap:', this.adventureMap.locations);
-
-      this.checkForNextGameState(this.activeGameStateSession);
     });
   }
 
@@ -78,44 +69,15 @@ export class GameStateManagerComponent implements OnInit {
   }
 
   populateGameSessionDisplay(gameCode: string) {
-    const parameter = "?gameCode=" + gameCode;
-
-    this.http
-      .get<GameSessionDisplay>(environment.nowhereBackendUrl + HttpConstants.DISPLAY_PATH + parameter)
+    this.gameService.getGameSessionDisplay(gameCode)
       .subscribe({
-        next: (response) => {
+        next: (response: any) => {
           console.log('Game session display retrieved from map!', response);
           
           this.gameSessionDisplay = response;
         },
-        error: (error) => {
+        error: (error: any) => {
           console.error('Error creating game', error);
-        },
-      });
-  }
-
-  checkForNextGameState(activeGameStateSession: ActiveGameStateSession) {
-    if (!this.isSettingNextGameState && activeGameStateSession.isPlayerDone.size !== 0) {
-      const allPlayersDone = Array.from(activeGameStateSession.isPlayerDone.values()).every(value => value);
-  
-      if (allPlayersDone) {
-        console.log('All players are done', activeGameStateSession);
-        this.isSettingNextGameState = true;
-        this.setToNextGameState();
-      }
-    }
-  }
-  
-  setToNextGameState() {
-    this.http
-      .put(environment.nowhereBackendUrl + HttpConstants.NEXT_GAME_SESSION_PATH + '?gameCode=' + this.gameCode, {})
-      .subscribe({
-        next: (response) => {
-          this.isSettingNextGameState = false;
-          console.log('Game phase updated', response);
-        },
-        error: (error) => {
-          console.error('Error updating game phase', error);
         },
       });
   }
--- src/services/game-session.service.ts
+++ src/services/game-session.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
 import { DocumentReference, Firestore, doc, docData } from '@angular/fire/firestore';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from '../environments/environments';
+import { HttpConstants } from '../assets/http-constants';
 
 @Injectable({ providedIn: 'root' })
 export class GameService {
-  constructor(private firestore: Firestore) {}
+  constructor(
+    private firestore: Firestore,
+    private http: HttpClient
+  ) {}
 
   listenForGameStateChanges(gameCode: string): Observable<{ gameState: string | null; didWeSucceed: boolean; activePlayerSession: any | null; activeGameStateSession: any | null }> {
     const gameDocRef: DocumentReference = doc(this.firestore, `gameSessions/${gameCode}`) as DocumentReference;
@@ -18,5 +24,10 @@ export class GameService {
         activeGameStateSession: data?.activeGameStateSession ?? null
       }))
     );
+  }
+
+  getGameSessionDisplay(gameCode: string): Observable<any> {
+    const parameter = "?gameCode=" + gameCode;
+    return this.http.get(environment.nowhereBackendUrl + HttpConstants.DISPLAY_PATH + parameter);
   }
 }
